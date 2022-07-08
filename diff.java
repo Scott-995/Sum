f48ae945858a3aa09d13d51a39a5fc98cd4bf440
Scott.k <scott.k19950@gmail.com>
2022-07-08 18:12:36 +0530

    initial commit

--- diff.java
+++ diff.java
@@ -3,6 +3,7 @@ public static void main(String[] args) {
     
     int a = 8;
     int b = 4;
+    
     System.out.println("the subtract is ="+(a-b));
     
 }
--- sum.java
+++ sum.java
@@ -3,8 +3,8 @@ public class sum {
     public static void main(String[] args) {
         int a=6;
         int b=4;
-        
-        System.out.println("the sum is = " + (a+b));
+        int c=0;
+        System.out.println("the sum is =" + (a+b+c));
         
     }
 
