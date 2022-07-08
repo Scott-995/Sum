05e15e5001c4f7e2cc3ff300850aee99a9490f9b
Scott.k <scott.k19950@gmail.com>
2022-07-08 15:32:21 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum {
+
+    public static void main(String[] args) {
+        int a=6;
+        int b=4;
+        
+        System.out.println("the sum is = " + (a+b));
+        
+    }
+
+
+}
